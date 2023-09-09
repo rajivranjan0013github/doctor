fcbb4e43dd5da71000f8249bbe769c6b30025723
Gourav <Gouravkumar.1273@gmail.com>
2023-09-09 19:25:11 +0530

    tratment-card

--- src/subcomponents/treatment/Treatments.jsx
+++ src/subcomponents/treatment/Treatments.jsx
@@ -1,19 +1,12 @@
 import React from "react";
 import services from "./treatmentData";
+import Card from "./Card";
 const Treatments = () => {
-  
+ 
   return (
-    <div className="container md:w-[93%] grid sm:grid-cols-2 lg:grid-cols-3 gap-3">
+    <div className="container md:w-[93%] grid sm:grid-cols-2 lg:grid-cols-3 gap-4 ">
       {services.map((element,index) => (
-        <div key={index} className=" text-left justify-center items-center bg-stone-100 flex flex-col p-5 font-[Roboto] rounded-3xl">
-          <img
-            className="w-[50px] h-[50px] self-center"
-            src={element.uri}
-            alt={element.title}
-          />
-          <h1 className="text-2xl font-bold">{element.title}</h1>
-          <p>{element.about}</p>
-        </div>
+        <Card element={element} index={index}/>
       ))}
     </div>
   );
